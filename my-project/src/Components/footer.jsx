977164151dcd142f4086b5ef348d3768234d214a
insha2472 <syedainshiraah84@gmail.com>
2026-02-11 12:19:49 +0530

    header and footer update

--- my-project/src/Components/footer.jsx
+++ my-project/src/Components/footer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const footer = () => {
   return (
     <div>
-        footer
+        <footer>
+            <p><footer style={{ padding: '1rem', backgroundColor: '#333', marginBottom: '1rem' }}></footer> </p>
+        </footer>
         </div>
   )
 }
